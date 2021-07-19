d422acd8da64c130ccf647c998cd1d71ae618c94
yansongda <me@yansongda.cn>
2021-07-19 18:11:15 +0800

    update

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -6,7 +6,7 @@ return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'kms'),
+        'database' => env('DB_DATABASE', 'yansongda'),
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', 'root'),
